7038269f455b540ae76d0196ef0cba69af5b9753
Azer <imranovazer@gmail.com>
2022-07-27 22:11:44 +0400

    changes2

--- src/navbar.js
+++ src/navbar.js
@@ -5,7 +5,7 @@ export default function  NavBar(props)
 
     return (
         <div className="navbar">
-            <div className="logo">MyNotes</div>
+            <div className="logo">MyNotes(beta)</div>
             <div className="addButton" onClick={props.funprop}> + </div>
         </div>
 
